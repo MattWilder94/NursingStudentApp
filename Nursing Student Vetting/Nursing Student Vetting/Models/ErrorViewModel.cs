db5b51aa8193fff4f8096628cb0fcd057354d54f
Seth Watson <spwatson@senators.ws.edu>
2025-01-21 09:28:21 -0500

    Initial Commit

---
+++ Nursing Student Vetting/Nursing Student Vetting/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Nursing_Student_Vetting.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
