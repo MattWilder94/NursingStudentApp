8b9e318eb0aff8efcadf8deff26d4fa352f626f8
Matthew Wilder <mbwilder1@senators.ws.edu>
2025-02-03 19:39:14 -0500

    created all models from entity relationship

---
+++ Nursing Student Vetting/Nursing Student Vetting/Models/StudentTest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Nursing_Student_Vetting.Models
+{
+    public class StudentTest
+    {
+        // primary key
+        public int TestID { get; set; }
+
+        // foreign key
+        public int StudentID { get; set; }
+
+        [Required(ErrorMessage = "Please enter a test score")]
+        public int TestScore { get; set; }
+        
+        public int AttemptNumber { get; set; }
+    }
+}
---
+++ Nursing Student Vetting/Nursing Student Vetting/Models/Test.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Nursing_Student_Vetting.Models
+{
+    public class Test
+    {
+        public int TestID { get; set; }
+
+        [Required(ErrorMessage = "Please enter a test name")]
+        public string TestName { get; set; } = String.Empty;
+
+        [Required(ErrorMessage = "Please enter a grading scale")]
+        public string GradingScale { get; set; } = String.Empty;
+    }
+}
