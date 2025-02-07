543b4a06805bd7cd14fa90cf0823a6b55df448e7
Mbwilder1 <mbwilder1@senators.ws.edu>
2025-02-07 15:44:49 -0500

    Added add and update controllers

--- Nursing Student Vetting/Nursing Student Vetting/Controllers/StudentsController.cs
+++ Nursing Student Vetting/Nursing Student Vetting/Controllers/StudentsController.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Nursing_Student_Vetting.Models;
 
 namespace Nursing_Student_Vetting.Controllers
 {
     public class StudentsController : Controller
     {
         private readonly NursingStudentContext _context;
+        private List<Student> students;
 
         public StudentsController(NursingStudentContext context)
         {
@@ -15,6 +18,53 @@ namespace Nursing_Student_Vetting.Controllers
         {
             var students = _context.Students.ToList();
             return View(students);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            Student student = new Student();
+
+            ViewBag.Student = student;
+
+            return View(student);
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            Student student = _context.Students  // returning student name and ID
+                .Include(s => s.FirstName)
+                .Include(s => s.LastName)
+                .FirstOrDefault(s => s.StudentID == id) ?? new Student();
+
+            ViewBag.Student = student;
+
+            return View(student);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Student student) 
+        {
+            if (ModelState.IsValid)
+            {
+                if (student.StudentID == 0)        // if student is new
+                {
+                    _context.Students.Add(student);
+                }
+                else
+                {
+                    _context.Students.Update(student);  // update existing student
+                }
+                _context.SaveChanges();
+                return RedirectToAction("List");
+            }
+            else
+            {
+                ViewBag.Student = students;
+
+                return View("List", student); // "List" will probably be changed to addstudent.html
+            }
         }
     }
 }
